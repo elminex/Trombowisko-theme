4f59f585986fdb12eb952f59167c1026af76db6e
Paweł Tuliński <pawel.tulinski@gmail.com>
2020-01-10 15:50:49 +0100

    Fix js and buttons

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -5,9 +5,11 @@
   const menuButton = document.querySelector('[data-button="menu"]');
   const subMenuTrigger = document.querySelectorAll('.menu-item-has-children');
   const pageBody = document.querySelector('body');
-  function toggleSub(e) {
-    e.currentTarget.classList.contains('sub-visible') ? e.currentTarget.classList.remove('sub-visible') : e.currentTarget.classList.add('sub-visible');
+
+  function toggleSub(event) {
+    event.currentTarget.classList.contains('sub-visible') ? e.currentTarget.classList.remove('sub-visible') : e.currentTarget.classList.add('sub-visible');
   };
+
   function toggleMenu() {
     if (pageBody.classList.contains('menu-visible')) {
       pageBody.classList.remove('menu-visible');
@@ -52,8 +54,9 @@
 
 function teacherBlockHandler() {
   const teacherButtons = document.querySelectorAll('[data-button="teacher"]');
-  const toggleActive = (e) => {
-    e.currentTarget.parentNode.parentNode.classList.toggle('active');
+  const toggleActive = (event) => {
+    const content = event.currentTarget.parentNode.parentNode;
+    content.classList.contains('active') ? content.classList.remove('active') : content.classList.add('active');
   }
   teacherButtons.forEach((button) => button.addEventListener('click', toggleActive))
 }
@@ -75,13 +78,13 @@ function offerBlockHandler() {
         const activeId = activeTab.getAttribute('data-to');
         const activeContent = block.querySelector(`#${activeId}`);
         const newTab = clickTarget;
-        const newContent = block.querySelector(`#${newTab.dataset.to}`); 
+        const newContent = block.querySelector(`#${newTab.dataset.to}`);
         activeTab.classList.remove('active');
         activeContent.classList.remove('active');
         currentActive = clickTarget;
         currentActive.classList.add('active');
         newContent.classList.add('active');
       }
-    }))
+    }));
   }
 }
